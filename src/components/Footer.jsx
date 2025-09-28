d633c3890e01b4ecd40e79063dfa0686989f2ec1
Korisnik <vmladenovic1524rn@raf.rs>
2025-09-28 13:54:54 +0200

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="w-full bg-blue-600 text-white py-4 mt-10 ">
             <div className="text-center text-sm">
-                © 2025 Dragan Gavric SV84-2024
+                © {new Date().getFullYear()} Veljko Mladenovic. All rights reserved.
             </div>
         </footer>
     );
